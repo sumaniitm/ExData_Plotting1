5a812cd5b9a3e21279b7ffdda03b700c067b762a
Suman Gangopadhyay <suman.gangopadhyay@noodle.ai>
2018-03-01 16:21:11 +0530

    added 3rd plot

---
+++ plot3.R
@@ -0,0 +1,10 @@
+library(data.table)
+hpc <- fread("household_power_consumption.txt")
+hpc_sub <- hpc[which(hpc$Date == "1/2/2007" | hpc$Date == "2/2/2007"),]
+hpc_sub$Datetime <- as.POSIXct(paste(hpc_sub$Date, hpc_sub$Time), format = "%d/%m/%Y %H:%M:%S")
+plot(hpc_sub$Datetime,hpc_sub$Sub_metering_1,ylab="Energy sub metering",xlab="",type="S",col="black")
+lines(hpc_sub$Datetime,hpc_sub$Sub_metering_2,col="red")
+lines(hpc_sub$Datetime,hpc_sub$Sub_metering_3,col="blue")
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col=c("black","red","blue"))
+dev.copy(png,"plot3.png", width=480, height=480)
+dev.off()
